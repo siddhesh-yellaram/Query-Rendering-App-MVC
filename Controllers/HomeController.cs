0d9420e853f49ceb3350a6ada5a06be1ed776f32
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 02:33:20 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QueryRenderingApp.Controllers
+{
+    public class HomeController : Controller
+    { 
+        //GET: Query
+        public ActionResult Query()
+        {
+            ViewBag.Name = Request.QueryString["developer"];
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QueryRenderingApp.MvcApplication" Language="C#" %>
